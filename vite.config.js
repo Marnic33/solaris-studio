82904324ba327372df1db0bfa5019b6f5f77ed45
Marnic33 <marcio.fat@gmail.com>
2026-08-05 21:17:45 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     outDir: 'dist',
+    /* alcança celulares um pouco mais antigos sem perder desempenho */
+    target: ['es2019', 'chrome80', 'safari13'],
     chunkSizeWarningLimit: 1200
   },
   server: {
